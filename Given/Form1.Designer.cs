47eb2fd336bc6514dee88028bdae52618cf3c6b6
mico210032 <48892947+mico210032@users.noreply.github.com>
2019-07-18 01:32:56 +0800

    Add files via upload

---
+++ Given/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Given
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "進入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(479, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 72);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "出去";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "08:00~16:00",
+            "00:00~12:00",
+            "12:00~23:59",
+            "00:00~23:59",
+            ""});
+            this.comboBox1.Location = new System.Drawing.Point(124, 220);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(289, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 72);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "選擇時段";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Given/Form1.cs
@@ -0,0 +1,193 @@
+﻿
+using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Given
+{
+ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // private const string FILE_NAME = "123.txt";
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var datetime = DateTime.Now;
+            StreamWriter sw = new StreamWriter("123.txt", true);
+            sw.WriteLine("進入時間為"+datetime+"\r");
+            sw.Close();           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+                var datetime = DateTime.Now;
+                StreamWriter sw = new StreamWriter("123.txt", true);
+                sw.WriteLine("出去時間為" + datetime + "\r");
+                sw.Close();
+            }
+        int a,b,c = 0; //a=總和 , b=時間 , c=比較
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamReader str = new StreamReader("123.txt");
+            string input;
+            //case 1
+            if (comboBox1.Text=="08:00~16:00")
+            {
+               string[]t= { "上午 08", "上午 09", "上午 10", "上午 11", "下午 12", "下午 01", "下午 02", "下午 03", "下午 04" };
+                 for(int i=0;i<9;i++)
+                {
+                    str.BaseStream.Seek(0, SeekOrigin.Begin);
+                    while ((input=str.ReadLine())!=null)
+                        {
+                            if(input.IndexOf(t[i])!=-1)
+                            {
+                        
+                                    c++;
+                            }
+                    
+                    }
+                       str.ReadToEnd();
+                    if (c>a)
+                        {
+                        a = c;
+                        b = i;
+                        c = 0;
+                        }
+                         c = 0;
+                       if(a>0)
+                    { 
+                    label1.Text = "最多時段為" + t[b]+"共"+a+ "人";
+                    }
+                       if(a==0)
+                    {
+                        label1.Text = "無此紀錄";
+                    }
+                } 
+                
+            }
+            //case 2
+            a = 0;
+            b = 0;
+            c = 0;
+            if (comboBox1.Text == "00:00~12:00")
+            {
+                string[] t = { "上午 00", "上午 01", "上午 02", "上午 03", "上午 04", "上午 05", "上午 06", "上午 07", "上午 08", "上午 09", "上午 10", "上午 11" };
+                for (int i = 0; i < 12; i++)
+                {
+                    str.BaseStream.Seek(0, SeekOrigin.Begin);
+                    while ((input = str.ReadLine()) != null)
+                    {
+                        if (input.IndexOf(t[i]) != -1)
+                        {
+
+                            c++;
+                        }
+
+                    }
+                    str.ReadToEnd();
+                    if (c > a)
+                    {
+                        a = c;
+                        b = i;
+                        c = 0;
+                    }
+                    c = 0;
+                    if (a > 0)
+                    {
+                        label1.Text = "最多時段為" + t[b] + "共" + a + "人";
+                    }
+                    if (a == 0)
+                    {
+                        label1.Text = "無此紀錄";
+                    }
+                }
+
+            }
+            //case 3 
+                 a = 0;
+                b = 0;
+                c = 0;
+            if (comboBox1.Text == "12:00~23:59")
+            {
+                string[] t = { "下午 12", "下午 01", "下午 02", "下午 03", "下午 04", "下午 05", "下午 06", "下午 07", "下午 08", "下午 09", "下午 10", "下午 11" };
+                for (int i = 0; i < 12; i++)
+                {
+                    str.BaseStream.Seek(0, SeekOrigin.Begin);
+                    while ((input = str.ReadLine()) != null)
+                    {
+                        if (input.IndexOf(t[i]) != -1)
+                        {
+
+                            c++;
+                        }
+
+                    }
+                    str.ReadToEnd();
+                    if (c > a)
+                    {
+                        a = c;
+                        b = i;
+                        c = 0;
+                    }
+                    c = 0;
+                    if (a > 0)
+                    {
+                        label1.Text = "最多時段為" + t[b] + "共" + a + "人";
+                    }
+                    if (a == 0)
+                    {
+                        label1.Text = "無此紀錄";
+                    }
+                }
+
+            }
+            //case4 
+            a = 0;
+            b = 0;
+            c = 0;
+            if (comboBox1.Text == "00:00~23:59")
+            {
+                string[] t = { "上午 00", "上午 01", "上午 02", "上午 03", "上午 04", "上午 05", "上午 06", "上午 07", "上午 08", "上午 09", "上午 10", "上午 11", "下午 12", "下午 01", "下午 02", "下午 03", "下午 04", "下午 05", "下午 06", "下午 07", "下午 08", "下午 09", "下午 10", "下午 11" };
+                for (int i = 0; i < 24; i++)
+                {
+                    str.BaseStream.Seek(0, SeekOrigin.Begin);
+                    while ((input = str.ReadLine()) != null)
+                    {
+                        if (input.IndexOf(t[i]) != -1)
+                        {
+
+                            c++;
+                        }
+
+                    }
+                    str.ReadToEnd();
+                    if (c > a)
+                    {
+                        a = c;
+                        b = i;
+                        c = 0;
+                    }
+                    c = 0;
+                    if (a > 0)
+                    {
+                        label1.Text = "最多時段為" + t[b] + "共" + a + "人";
+                    }
+                    if (a == 0)
+                    {
+                        label1.Text = "無此紀錄";
+                    }
+                }
+               
+            } 
+
+        }
+        
+    }
+    }
+
